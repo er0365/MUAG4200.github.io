d1b2b29736b9ef6d40f55b3c8e4ae0608d7657d7
er0365 <84043471+er0365@users.noreply.github.com>
2021-05-21 13:18:08 -0500

    adding back ambient sounds

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,6 +52,11 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("thunderwall.wav");
 soundArray.push(scoreSound);
 
+export var ambientSound = new soundFile("theraincomescomplete.wav");
+soundArray.push(ambientSound);
+ambientSound.player.loop = true; //turn on looping
+ambientSound.player.volume.value = -20; //turn down volume
+
 export var adventureMusic = new soundFile("theraincomescomplete.wav");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
