57cc9ebd73410c644681b685c1fbee77ea0d33da
er0365 <84043471+er0365@users.noreply.github.com>
2021-05-21 03:16:58 -0500

    put in new bg music

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,17 +52,17 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("rainwall.wav");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("theraincomes.wav");
+export var ambientSound = new soundFile("theraincomescomplete.wav");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -20; //turn down volume
 
-export var adventureMusic = new soundFile("theraincomes.wav");
+export var adventureMusic = new soundFile("theraincomescomplete.wav");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
 adventureMusic.player.volume.value = -16;
 
-export var villageMusic = new soundFile("theraincomes.wav");
+export var villageMusic = new soundFile("theraincomescomplete.wav");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
 villageMusic.player.volume.value = -16;
