97d7cdd73f62f98d35bb6222d836c070d45c1f7d
er0365 <84043471+er0365@users.noreply.github.com>
2021-05-21 11:36:28 -0500

    change all bg to -20?

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -55,14 +55,14 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("theraincomescomplete.wav");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -16; //turn down volume
+ambientSound.player.volume.value = -20; //turn down volume
 
 export var adventureMusic = new soundFile("theraincomescomplete.wav");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
-adventureMusic.player.volume.value = -16;
+adventureMusic.player.volume.value = -20;
 
 export var villageMusic = new soundFile("theraincomescomplete.wav");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
-villageMusic.player.volume.value = -16;
+villageMusic.player.volume.value = -20;
