9913484d5bdce0f1280144b1fa15ccacda73ee75
er0365 <84043471+er0365@users.noreply.github.com>
2021-05-21 18:15:27 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,10 +52,10 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("thunderwall.wav");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("theraincomescomplete.wav");
+/*export var ambientSound = new soundFile("theraincomescomplete.wav");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -20; //turn down volume
+ambientSound.player.volume.value = -20; //turn down volume*/
 
 export var adventureMusic = new soundFile("theraincomescomplete.wav");
 soundArray.push(adventureMusic);
