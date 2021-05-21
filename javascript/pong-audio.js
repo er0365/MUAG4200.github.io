9f9211020fc3a49e192bb771e227a2a4856a2be2
er0365 <84043471+er0365@users.noreply.github.com>
2021-05-21 01:33:19 -0500

    added new sounds to audiojs

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -43,10 +43,10 @@ export var soundArray = []; //list of sounds loaded
 
 //Here is where all the Sound File Players Start
 
-export var wallSound = new soundFile("tap-resonant.mp3"); //load sound
+export var wallSound = new soundFile("pongwall.wav"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
-export var paddleSound = new soundFile("paddle.wav");
+export var paddleSound = new soundFile("pongpaddle.wav");
 soundArray.push(paddleSound);
 
 export var scoreSound = new soundFile("phaserDown2.mp3");
